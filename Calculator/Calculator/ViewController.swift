2e025c737e9fb6dc0123ee0dccc8c533e95ec94a
Alexandre Malkov <sm1030@gmail.com>
2016-12-04 21:05:54 +0000

    Lesson 1

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Alexandre Malkov on 04/12/2016.
+//  Copyright © 2016 Alexandre Malkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsInTheMiddleOfTyping = false
+
+    @IBAction func touchDigit(_ sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsInTheMiddleOfTyping {
+            let textCurrentlyInDisplay = display.text!
+            display.text = textCurrentlyInDisplay + digit
+        } else {
+            display.text = digit
+        }
+        userIsInTheMiddleOfTyping = true
+    }
+
+
+    @IBAction func performOperation(_ sender: UIButton) {
+        userIsInTheMiddleOfTyping = false
+        if let mathematicalSymbol = sender.currentTitle {
+            if mathematicalSymbol == "π" {
+                display.text = String(M_PI)
+            }
+        }
+    }
+
+}
+
